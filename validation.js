4a565903d809d68c52bdd22bf30caf5704c0b34b
pawelNobis <pawel.nobis1@gmail.com>
2019-02-13 20:33:15 +0100

    validation.js file corrected

--- validation.js
+++ validation.js
@@ -4,7 +4,7 @@ const inputs = document.querySelectorAll('input');
 const patterns = {
     username: /^[a-z\d]{5,12}$/i,
     email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
-    password: /^ [\w@-]{ 8, 20 } $ /,
+    password: /^[\w@-]{8,20}$/,
     telephone: /^\d{11}$/,
     slug: /^[a-z\d-]{8,20}$/
 };
